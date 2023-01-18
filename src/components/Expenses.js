3c8f6029a688c3e971db6adce712d00bfed8579a
simplyatul <atulthosar@gmail.com>
2023-01-18 17:06:23 +0530

    nested component done

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,9 +1,10 @@
 import "./Expenses.css";
 import ExpenseItem from "./ExpenseItem";
+import Card from "./Card";
 
 function Expenses(props) {
   return (
-    <div className="expenses">
+    <Card className="expenses">
       <ExpenseItem
         date={props.item[0].date}
         title={props.item[0].title}
@@ -15,7 +16,7 @@ function Expenses(props) {
         title={props.item[1].title}
         amount={props.item[1].amount}
       />
-    </div>
+    </Card>
   );
 }
 
