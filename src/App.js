ff6e9ded8c12157afdebf628707dfc391c260688
simplyatul <atulthosar@gmail.com>
2023-01-18 12:02:33 +0530

    displaying expenses

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+import ExpenseItem from './components/ExpenseItem'
 
-function App() {
-  // Following is JSX - Java Script XML
+function App () {
+
+  let expenses = [
+    {
+      id: 'e1',
+      title: 'Schoole Fee',
+      amount: 300,
+      date: new Date(2022, 1, 18)
+    },
+    {
+      id: 'e2',
+      title: 'Rent',
+      amount: 7000,
+      date: new Date(2022, 1, 15)
+    },
+    {
+      id: 'e3',
+      title: 'Books',
+      amount: 840,
+      date: new Date(2022, 1, 1)
+    },
+    {
+      id: 'e4',
+      title: 'Food',
+      amount: 500,
+      date: new Date(2022, 1, 18)
+    }
+  ];
+  let h2Hdr = 'Let us start !!!';
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div> 
+        <h2>
+          { h2Hdr }
+        </h2>
+        <ExpenseItem 
+          date={expenses[0].date} 
+          title={expenses[0].title} 
+          amount={expenses[0].amount}>
+        </ExpenseItem> 
+
+        <ExpenseItem 
+          date={expenses[1].date} 
+          title={expenses[1].title} 
+          amount={expenses[1].amount}>
+        </ExpenseItem> 
+
+      </div>
   );
+
 }
 
 export default App;
+
+
+
