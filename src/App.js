3e978adce139b20dfc101898815220ee578e1587
simplyatul <atulthosar@gmail.com>
2023-01-18 18:18:21 +0530

    state mgmt. Issue: title not changing on button click.

--- src/App.js
+++ src/App.js
@@ -29,6 +29,18 @@ const App = () => {
     },
   ];
   let h2Hdr = "Let us start !!!";
+
+  // console.log("static expenses: " + expenses);
+  // fetch("https://retoolapi.dev/BzV4YN/data")
+  //   .then((response) => {
+  //     return response.json();
+  //   })
+  //   .then((data) => {
+  //     console.log(data);
+  //     expenses = data;
+  //     console.log("fetched expenses: " + expenses);
+  //   });
+
   return (
     <div>
       <h2>{h2Hdr}</h2>
--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -4,13 +4,20 @@ import Card from "../UI/Card";
 import React from "react";
 
 const ExpenseItem = (props) => {
+  let title = props.title;
+  const clickHnandler = () => {
+    title = "New Title..";
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2> {props.title} </h2>
+        <h2> {title} </h2>
         <h2 className="expense-item__price"> $ {props.amount} </h2>
       </div>
+
+      <button onClick={clickHnandler}> Change Title </button>
     </Card>
   );
 };
