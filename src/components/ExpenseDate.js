6841efcfc7af3161689496d1b774c95ba5ae2937
simplyatul <atulthosar@gmail.com>
2023-01-18 16:30:17 +0530

    css applied

--- src/components/ExpenseDate.js
+++ src/components/ExpenseDate.js
@@ -6,10 +6,10 @@ function ExpenseDate(props) {
   const day = props.date.toLocaleString("en-US", { day: "2-digit" });
 
   return (
-    <div>
-      <div> {month} </div>
-      <div> {year} </div>
-      <div> {day} </div>
+    <div className="expense-date">
+      <div className="expense-date__month"> {month} </div>
+      <div className="expense-date__year"> {year} </div>
+      <div className="expense-date__day"> {day} </div>
     </div>
   );
 }
