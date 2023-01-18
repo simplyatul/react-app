d424900a4d969e5baeebe7bce4bf017e35be1723
simplyatul <atulthosar@gmail.com>
2023-01-18 16:27:58 +0530

    before applying css

--- src/App.js
+++ src/App.js
@@ -1,59 +1,49 @@
-// import logo from './logo.svg';
-// import './App.css';
-import ExpenseItem from './components/ExpenseItem'
+import ExpenseItem from "./components/ExpenseItem";
 
-function App () {
-
+function App() {
   let expenses = [
     {
-      id: 'e1',
-      title: 'Schoole Fee',
-      amount: 300,
-      date: new Date(2022, 1, 18)
-    },
-    {
-      id: 'e2',
-      title: 'Rent',
-      amount: 7000,
-      date: new Date(2022, 1, 15)
-    },
-    {
-      id: 'e3',
-      title: 'Books',
-      amount: 840,
-      date: new Date(2022, 1, 1)
-    },
-    {
-      id: 'e4',
-      title: 'Food',
+      id: "e1",
+      title: "Schoole Fee",
       amount: 500,
-      date: new Date(2022, 1, 18)
-    }
+      date: new Date(2022, 1, 18),
+    },
+    {
+      id: "e2",
+      title: "Rent",
+      amount: 7000,
+      date: new Date(2022, 1, 15),
+    },
+    {
+      id: "e3",
+      title: "Books",
+      amount: 840,
+      date: new Date(2022, 1, 1),
+    },
+    {
+      id: "e4",
+      title: "Food",
+      amount: 500,
+      date: new Date(2022, 1, 18),
+    },
   ];
-  let h2Hdr = 'Let us start !!!';
+  let h2Hdr = "Let us start !!!";
   return (
-      <div> 
-        <h2>
-          { h2Hdr }
-        </h2>
-        <ExpenseItem 
-          date={expenses[0].date} 
-          title={expenses[0].title} 
-          amount={expenses[0].amount}>
-        </ExpenseItem> 
+    <div>
+      <h2>{h2Hdr}</h2>
+      <ExpenseItem
+        date={expenses[0].date}
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+      ></ExpenseItem>
 
-        <ExpenseItem 
-          date={expenses[1].date} 
-          title={expenses[1].title} 
-          amount={expenses[1].amount}>
-        </ExpenseItem> 
-
-      </div>
+      <ExpenseItem
+        date={expenses[1].date}
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+      ></ExpenseItem>
+    </div>
   );
-
 }
 
 export default App;
-
-
-
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,23 +1,16 @@
-import './ExpenseItem.css'
+import ExpenseDate from "./ExpenseDate";
+import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
-  const month = props.date.toLocaleString('en-US', {month: 'long'});
-  const year = props.date.getFullYear();
-  const day = props.date.toLocaleString('en-US', {day: '2-digit'});
   return (
-    <div className='expense-item'>
-       <div>
-        <div> { month } </div>
-        <div> { year } </div>
-        <div> { day } </div>
-      </div>
-      <div className='expense-item__description'>
-        <h2> { props.title } </h2>
-        <h2 className='expense-item__price'> $ { props.amount } </h2>
+    <div className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2> {props.title} </h2>
+        <h2 className="expense-item__price"> $ {props.amount} </h2>
       </div>
     </div>
   );
 }
 
 export default ExpenseItem;
-
