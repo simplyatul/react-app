2c8d2fbd7b17e01e594e19ed2ee016103b13babe
simplyatul <atulthosar@gmail.com>
2023-01-19 10:26:41 +0530

    input handling

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -4,10 +4,16 @@ import Card from "../UI/Card";
 import React, { useState } from "react";
 
 const ExpenseItem = (props) => {
+  const [newTitle, setNewTitle] = useState("Hi");
+
   const [title, setTitle] = useState(props.title);
 
   const clickHnandler = () => {
-    setTitle("New Title..");
+    setTitle(newTitle);
+  };
+
+  const changeHandler = (event) => {
+    setNewTitle(event.target.value);
   };
 
   return (
@@ -18,6 +24,7 @@ const ExpenseItem = (props) => {
         <h2 className="expense-item__price"> $ {props.amount} </h2>
       </div>
 
+      <input type="text" value={newTitle} onChange={changeHandler} />
       <button onClick={clickHnandler}> Change Title </button>
     </Card>
   );
